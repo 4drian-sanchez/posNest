069e807adcda790b4fedc7e2bda8d5ffa00b7d45
Adrian <adrian.s.duque@gmail.com>
2025-03-11 17:45:14 -0400

    funcion de seeder agregada

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  //Se agrega app.useGlobalPipes para que cumpla con lo que esta en los dto
-  //Se necesitan dos dependencias: npm i class-validator class-transformer
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useGlobalPipes( new ValidationPipe({
     whitelist: true
   }))
+  app.useStaticAssets(join(__dirname, '../public'))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Category } from 'src/categories/entities/category.entity';
+import { Category } from '../categories/entities/category.entity';
 import { FindManyOptions, Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 
